f5a7493a4231eb3fcf25bcf15ba4d4658f5d212a
michael <michael_wang90@163.com>
2019-10-25 19:20:31 +0800

    init frame starter and social starter

---
+++ social-spring-boot-starter/social-spring-boot-starter-autoconfigure/src/main/java/net/yunqihui/starter/social/SocialAutoConfiguration.java
@@ -0,0 +1,16 @@
+//package net.yunqihui.starter.social;
+//
+//import org.springframework.boot.autoconfigure.condition.ConditionalOnWebApplication;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * @Description
+// * @Author Michael Wong
+// * @Email michael_wong@yunqihui.net
+// * @Date 2019/10/23 18:13
+// **/
+//@Configuration
+//@ConditionalOnWebApplication  // web应用才生效
+//public class SocialAutoConfiguration {
+//
+//}
