87648d060abae570a1ddad6c7c6aef29bb6da162
michael <michael_wang90@163.com>
2019-10-28 17:41:43 +0800

    fix autoconfigBean can not scan

--- social-spring-boot-starter/social-spring-boot-starter-autoconfigure/src/main/java/net/yunqihui/SocialSpringBootStarterAutoconfigureApplication.java
+++ social-spring-boot-starter/social-spring-boot-starter-autoconfigure/src/main/java/net/yunqihui/SocialSpringBootStarterAutoconfigureApplication.java
@@ -1,19 +1,19 @@
-package net.yunqihui.starter.social;
-
-import org.mybatis.spring.annotation.MapperScan;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.cache.annotation.EnableCaching;
-import org.springframework.transaction.annotation.EnableTransactionManagement;
-
-@SpringBootApplication
-@MapperScan("net.yunqihui.starter.social.mapper")
-@EnableCaching
-@EnableTransactionManagement // 开启事物支持，一般不显式声明也可以，但是建议声明一下
-public class SocialSpringBootStarterAutoconfigureApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(SocialSpringBootStarterAutoconfigureApplication.class, args);
-	}
-
-}
+//package net.yunqihui.starter.social;
+//
+//import org.mybatis.spring.annotation.MapperScan;
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//import org.springframework.cache.annotation.EnableCaching;
+//import org.springframework.transaction.annotation.EnableTransactionManagement;
+//
+//@SpringBootApplication
+//@MapperScan("net.yunqihui.starter.social.mapper")
+//@EnableCaching
+//@EnableTransactionManagement // 开启事物支持，一般不显式声明也可以，但是建议声明一下
+//public class SocialSpringBootStarterAutoconfigureApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(SocialSpringBootStarterAutoconfigureApplication.class, args);
+//	}
+//
+//}
