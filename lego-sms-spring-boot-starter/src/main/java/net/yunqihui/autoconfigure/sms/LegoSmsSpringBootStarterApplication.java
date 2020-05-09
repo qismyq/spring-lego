294c05e0e4efb6a9b7b74f751c0bced533b4fca6
michael <michael_wang90@163.com>
2020-05-09 12:56:06 +0800

    main

---
+++ lego-sms-spring-boot-starter/src/main/java/net/yunqihui/autoconfigure/sms/LegoSmsSpringBootStarterApplication.java
@@ -0,0 +1,13 @@
+package net.yunqihui.autoconfigure.sms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoSmsSpringBootStarterApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoSmsSpringBootStarterApplication.class, args);
+	}
+
+}
--- lego-wechat-spring-boot-starter/src/main/java/net/yunqihui/LegoWeChatSpringBootStarterApplication.java
+++ lego-wechat-spring-boot-starter/src/main/java/net/yunqihui/LegoWeChatSpringBootStarterApplication.java
@@ -1,17 +1,17 @@
-//package net.yunqihui;
-//
-//import org.mybatis.spring.annotation.MapperScan;
-//import org.springframework.boot.SpringApplication;
-//import org.springframework.boot.autoconfigure.SpringBootApplication;
-//import org.springframework.cache.annotation.EnableCaching;
-//
-//@SpringBootApplication
-//@MapperScan("net.yunqihui.**.mapper")
-//@EnableCaching
-//public class LegoWeChatSpringBootStarterApplication {
-//
-//    public static void main(String[] args) {
-//        SpringApplication.run(LegoWeChatSpringBootStarterApplication.class, args);
-//    }
-//
-//}
+package net.yunqihui;
+
+import org.mybatis.spring.annotation.MapperScan;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+
+@SpringBootApplication
+@MapperScan("net.yunqihui.**.mapper")
+@EnableCaching
+public class LegoWeChatSpringBootStarterApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LegoWeChatSpringBootStarterApplication.class, args);
+    }
+
+}
