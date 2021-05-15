705ab8d5ee07b30eec9248a888ae8061a827682a
qismyq <michael_wang90@163.com>
2021-05-15 15:05:25 +0800

    完善短信服务属性获取方式

--- lego-sms-spring-boot-starter/src/main/java/net/yunqihui/autoconfigure/sms/entity/AliyunSMSChanel.java
+++ lego-sms-spring-boot-starter/src/main/java/net/yunqihui/autoconfigure/sms/entity/AliyunSMSChanel.java
@@ -10,7 +10,8 @@ import com.aliyuncs.profile.IClientProfile;
 import lombok.extern.slf4j.Slf4j;
 import net.yunqihui.autoconfigure.frame.errorhandler.ErrorMessageException;
 import net.yunqihui.autoconfigure.sms.errorhandler.SMSErrorCodeEnum;
-import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 
 /**
@@ -20,27 +21,31 @@ import org.springframework.stereotype.Service;
  * @create: 2021-03-27 22:05
  **/
 @Slf4j
-@ConfigurationProperties(prefix = "lego.sms")
-@Service(value = "aliyun-sms")
-public class AliyunSMSChanel implements SMSChanel{
+@Service(value = "sms-aliyun")
+public class AliyunSMSChanel implements SMSChanel {
 
     private static String regionId = "cn-hangzhou";
     private static String endpointName = "cn-hangzhou";
     private static String product = "Dysmsapi";
     private static String domain = "dysmsapi.aliyuncs.com";
 
+    @Value("${lego.sms.accessKeyId:#{null}}")
     private String aliAccessKeyId;
+    @Value("${lego.sms.accessKeySecret:#{null}}")
     private String aliAccessKeySecret;
+    @Value("${lego.sms.signName:#{null}}")
     private String aliSignName;
 
 
-
     @Override
-    public void sendSMS(String mobile, String content,String templateCode) throws Exception {
+    public void sendSMS(String mobile, String content, String templateCode) throws Exception {
+        if (StringUtils.isBlank(aliAccessKeyId) || StringUtils.isBlank(aliAccessKeySecret) || StringUtils.isBlank(aliSignName)) {
+            throw new ErrorMessageException(SMSErrorCodeEnum.E_50201);
+        }
         JSONObject jsonObj = new JSONObject();
         jsonObj.put("code", content);
-        IClientProfile profile = DefaultProfile.getProfile(regionId,aliAccessKeyId , aliAccessKeySecret);
-        DefaultProfile.addEndpoint(endpointName, regionId, product,  domain);
+        IClientProfile profile = DefaultProfile.getProfile(regionId, aliAccessKeyId, aliAccessKeySecret);
+        DefaultProfile.addEndpoint(endpointName, regionId, product, domain);
         IAcsClient client = new DefaultAcsClient(profile);
         //组装请求对象-具体描述见控制台-文档部分内容
         SendSmsRequest request = new SendSmsRequest();
@@ -51,7 +56,7 @@ public class AliyunSMSChanel implements SMSChanel{
         SendSmsResponse sendSmsResponse = client.getAcsResponse(request);
         if (!"OK".equals(sendSmsResponse.getCode())) {
             log.error(sendSmsResponse.getMessage());
-            throw new ErrorMessageException(SMSErrorCodeEnum.E_50100.getErrorCode(), sendSmsResponse.getMessage());
+            throw new ErrorMessageException(SMSErrorCodeEnum.E_50200.getErrorCode(), sendSmsResponse.getMessage());
         }
     }
 }
