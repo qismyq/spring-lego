9981e317272235a01ef6f9d85ee98283d7f6fd2a
qismyq <michael_wang90@163.com>
2023-11-11 17:26:46 +0800

    pom部分重构

---
+++ lego-user-spring-boot-starter/src/main/java/com/springlego/autoconfigure/user/config/CaptchaConfig.java
@@ -0,0 +1,58 @@
+package com.springlego.autoconfigure.user.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Lazy;
+
+import java.awt.*;
+
+/**
+ * 验证码配置
+ * Thanks for Lion Li
+ * @author Lion Li
+ */
+@Configuration
+public class CaptchaConfig {
+
+    private static final int WIDTH = 150;
+    private static final int HEIGHT = 40;
+    private static final Color BACKGROUND = new Color(44,49,46);
+    private static final Font FONT = new Font("Arial", Font.BOLD, 48);
+
+    /**
+     * 圆圈干扰验证码
+     */
+    @Lazy
+    @Bean
+    public CircleCaptcha circleCaptcha() {
+        CircleCaptcha captcha = CaptchaUtil.createCircleCaptcha(WIDTH, HEIGHT);
+        captcha.setBackground(BACKGROUND);
+        captcha.setFont(FONT);
+        return captcha;
+    }
+
+    /**
+     * 线段干扰的验证码
+     */
+    @Lazy
+    @Bean
+    public LineCaptcha lineCaptcha() {
+        LineCaptcha captcha = CaptchaUtil.createLineCaptcha(WIDTH, HEIGHT);
+        captcha.setBackground(BACKGROUND);
+        captcha.setFont(FONT);
+        return captcha;
+    }
+
+    /**
+     * 扭曲干扰验证码
+     */
+    @Lazy
+    @Bean
+    public ShearCaptcha shearCaptcha() {
+        ShearCaptcha captcha = CaptchaUtil.createShearCaptcha(WIDTH, HEIGHT);
+        captcha.setBackground(BACKGROUND);
+        captcha.setFont(FONT);
+        return captcha;
+    }
+
+}
