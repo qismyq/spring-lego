f1f7571c5b26de5ca87ee1735f5c740d433bda38
H2018452 <H2018452>
2024-08-28 16:49:17 +0800

    表名称，及init sql

--- lego-common-spring-boot-starter/src/main/java/com/springlego/autoconfigure/common/entity/dataobject/dict/DictDataDO.java
+++ lego-common-spring-boot-starter/src/main/java/com/springlego/autoconfigure/common/entity/dataobject/dict/DictDataDO.java
@@ -11,8 +11,8 @@ import lombok.EqualsAndHashCode;
  *
  * @author ruoyi
  */
-@TableName("system_dict_data")
-@KeySequence("system_dict_data_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
+@TableName("dic_dict_data")
+@KeySequence("dic_dict_data_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class DictDataDO extends BaseDO {
--- lego-common-spring-boot-starter/src/main/java/com/springlego/autoconfigure/common/entity/dataobject/dict/DictTypeDO.java
+++ lego-common-spring-boot-starter/src/main/java/com/springlego/autoconfigure/common/entity/dataobject/dict/DictTypeDO.java
@@ -14,8 +14,8 @@ import java.time.LocalDateTime;
  *
  * @author ruoyi
  */
-@TableName("system_dict_type")
-@KeySequence("system_dict_type_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
+@TableName("dic_dict_type")
+@KeySequence("dic_dict_type_seq") // 用于 Oracle、PostgreSQL、Kingbase、DB2、H2 数据库的主键自增。如果是 MySQL 等数据库，可不写。
 @Data
 @EqualsAndHashCode(callSuper = true)
 @ToString(callSuper = true)
